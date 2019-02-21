868e9c751c0ac0c194395fe06192ed8c46abafe9
Lowry West <4lowrywest@gmail.com>
2019-02-20 23:11:21 -0600

    Inherit class

--- src/MesoInherit.java
+++ src/MesoInherit.java
@@ -1,1 +1,44 @@
-//TODO
+import java.util.Arrays;
+
+public class MesoInherit extends MesoAbstract
+{
+	String STIDval= "";
+	char[] vals= new char[4];
+	int[] charVals= new int[3];
+	
+	public MesoInherit(MesoStation Stid)
+	{
+		STIDval=Stid.getStID();
+		vals=STIDval.toCharArray();
+	}
+
+	@Override
+	public int[] calAverage() 
+	{
+		
+		Double avg= (vals[0]+vals[1]+vals[2]+vals[3])/4.0;
+		charVals[0]= (int)Math.ceil(avg);
+		charVals[1]= (int)Math.floor(avg);
+		
+			if(avg-Math.floor(avg)< .5)
+			{
+				charVals[2]= (int)Math.floor(avg);
+			}
+			else
+			{
+				charVals[2]= (int)Math.ceil(avg);
+			}
+		
+		System.out.println(Arrays.toString(charVals));
+		// TODO Auto-generated method stub
+		return charVals;
+	}
+
+	@Override
+	public char letterAverage() 
+	{
+		char val= (char)charVals[3];
+		return val;
+	}
+	//TODO
+}

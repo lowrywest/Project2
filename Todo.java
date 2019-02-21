31cc40e22e0e2588890c0f0800af7e2eee1bc3cb
Lowry West <4lowrywest@gmail.com>
2019-02-20 19:10:09 -0600

    Inital Files

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-19
+OU ID_______________: 113410037
+OU EMAIL____________: lowry.y.west-1@ou.edu
+
+Tasks:
+1. calculate two tokens
+2. calculate three tokens
+3. execute
+4. parse and excute
+5. calculator tests
+
+Estimated Time (minutes):
+1. 20
+2. 20
+3. 20
+4. 20
+5. 50
+
+Actual Time (minutes):
+1. 30
+2. 30
+3. 30
+4. 30
+5. 120
+*/}
